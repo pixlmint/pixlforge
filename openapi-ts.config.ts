5e9d7107cc4052b6ff0bb4ecb565fe7aa1db0c06
Christian Gröber <swiss8oy.chg@gmail.com>
2026-05-25 11:45:28 +0200

    feat: add packages to generate code for forgejo api communication

---
+++ openapi-ts.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+    input: 'https://git.pixlmint.ch/swagger.v1.json',
+    output: 'app/lib/api/generated',
+    plugins: [
+        '@hey-api/client-fetch',
+        '@tanstack/vue-query',
+    ],
+})
