a36dc38981919437f7de0b91fef0b23e703a2fa5
Christian Gröber <swiss8oy.chg@gmail.com>
2026-05-31 20:35:25 +0200

    chore: cache

--- server/api/recent/index.ts
+++ server/api/recent/index.ts
@@ -1,19 +1,22 @@
-import type { H3Event } from "h3";
-import { getIssues } from "./issues";
-import { getRepos } from "./repos";
-import { getActions } from "./actions";
-import { getCommits } from "./commits";
-import { getActivity } from "./activity";
+import type { H3Event } from 'h3'
+import { getIssues } from './issues'
+import { getRepos } from './repos'
+import { getActions } from './actions'
+import { getCommits } from './commits'
+import { getActivity } from './activity'
 
-export default defineEventHandler(async () => {
-    return {
-        issues: await getIssues(),
-        repos: await getRepos(),
-        actions: await getActions(),
-        commits: await getCommits(),
-        activity: await getActivity(),
-    }
-// }, {
-//     maxAge: 1,
-//     getKey: (event: H3Event) => event.path + 'v5',
-})
+export default defineCachedEventHandler(
+    async () => {
+        return {
+            issues: await getIssues(),
+            repos: await getRepos(),
+            actions: await getActions(),
+            commits: await getCommits(),
+            activity: await getActivity(),
+        }
+    },
+    {
+        maxAge: 1,
+        getKey: (event: H3Event) => event.path + 'v5',
+    },
+)
--- server/api/repo.ts
+++ server/api/repo.ts
@@ -1,15 +1,17 @@
-import { z } from "zod";
-import { repoGet } from "~~/lib/generated";
+import { z } from 'zod'
+import { repoGet } from '~~/lib/generated'
 
 const repoRequestSchema = z.object({
     repo: z.string(),
     owner: z.string().default('pixlmint'),
-});
+})
 
-export default cachedEventHandler(async (event) => {
-    const request = await getValidatedQuery(event, body => repoRequestSchema.parse(body));
+export default defineEventHandler(async (event) => {
+    const request = await getValidatedQuery(event, (body) =>
+        repoRequestSchema.parse(body),
+    )
 
-    return repoGet({ path: { repo: request.repo, owner: request.owner } });
-}, {
-    maxAge: 60 * 10,
-});
+    return repoGet({ path: { repo: request.repo, owner: request.owner } })
+    // }, {
+    //     maxAge: 60 * 10,
+})
