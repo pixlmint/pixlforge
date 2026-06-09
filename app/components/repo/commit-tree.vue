771176f16485c5b87353508b30ca555e41e1ab24
Christian Gröber <swiss8oy.chg@gmail.com>
2026-06-09 11:44:02 +0200

    fix: cleanup commit-tree.vue template, better styling of commit history

--- app/components/repo/commit-tree.vue
+++ app/components/repo/commit-tree.vue
@@ -1,29 +1,4 @@
 <template>
-    <a :href="`https://git.pixlmint.ch/${owner}/${repo}`">{{ owner }}/{{ repo }}</a>
-
-    <h2>Branches</h2>
-    <ul>
-        <li v-for="(branch, index) in branches.data" :key="index">
-            <ul>
-                <li>Name: {{ branch.name }}</li>
-                <li v-if="branch.commit !== undefined">
-                    <ul>
-                        <li>sha: {{ branch.commit.id }}</li>
-                        <li>message: {{ branch.commit.message }}</li>
-                        <li>timestamp: {{ branch.commit.timestamp }}</li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-    </ul>
-
-    <details>
-        <summary>commit example</summary>
-        <!-- <VueJsonPretty :data="branchCommits[0]!.commits[0]" /> -->
-    </details>
-
-    <h2>Commits</h2>
-
     <div class="commit-history-graph" :style="`--commit-history-item-height: ${LANE_HEIGHT}px`">
         <svg ref="graph"></svg>
         <div class="commit-history-labels">
@@ -35,63 +10,9 @@
             />
         </div>
     </div>
-
-    <details>
-        <summary>Raw</summary>
-        <div class="commit-list-split">
-            <table class="commit-list">
-                <thead>
-                    <tr>
-                        <th class="rotate" v-for="(_, index) in columns" :key="index">
-                            <div>
-                                <!-- <span>{{ id }}</span> -->
-                            </div>
-                        </th>
-                        <th>info</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="(commit, _) in commits"
-                        :key="commit.sha"
-                        :class="{
-                            active: viewingCommit !== null && viewingCommit.sha === commit.sha,
-                        }"
-                    >
-                        <td v-for="(_, index) in columns" :key="index">
-                            <span v-if="commit.column === index">
-                                <template v-if="commit.isMerge"> \ </template>
-                                <template v-else> * </template>
-                            </span>
-                            <span v-else> | </span>
-                        </td>
-                        <td class="commit-info" @click="viewingCommit = commit">
-                            <span class="commit-sha">{{ commit.sha.substring(0, 7) }}</span>
-                            <span class="commit-message">{{ commit.message.split('\n')[0]! }}</span>
-                            <span class="commit-date">
-                                <!-- <timeago :date="commit.timestamp" /> -->
-                            </span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <div class="commit-detail">
-                <vue-json-pretty
-                    class="commit-detail-content"
-                    v-if="viewingCommit !== null"
-                    :data="viewingCommit"
-                />
-            </div>
-        </div>
-        <div class="commit-graph">
-            <repo-graph-entry v-for="(commit, _) in commits" :key="commit.sha" :commit="commit" />
-        </div>
-    </details>
 </template>
 
 <script lang="ts" setup>
-import VueJsonPretty from 'vue-json-pretty'
 import * as d3 from 'd3'
 import type { HistoryCommit } from '~~/shared/types'
 
@@ -99,14 +20,10 @@ const { owner, repo } = defineProps<{ owner: string; repo: string }>()
 
 const { branches, commits } = (await useFetch(`/api/${owner}/${repo}/commit-tree`)).data!.value!
 
-const columns = new Array(Math.max(...commits.map((commit) => commit.column!)) + 1)
-
 const graph = ref(null)
 
 const LANE_HEIGHT = 24 // 24px per lane
 const LANE_WIDTH = 15 // 10px per lane
-
-const viewingCommit = ref<HistoryCommit | null>(null)
 
 type Point = {
     x: number
@@ -188,75 +105,33 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
-// .commit-list {
-//     display: flex;
-// }
-
-.commit-graph {
-    display: grid;
-    grid-template-columns: 20px auto;
-}
-
-.commit-list-split {
-    display: flex;
-
-    table.commit-list {
-        width: 75%;
-
-        .commit-info {
-            span {
-                margin: 0 5px;
-            }
-
-            .commit-sha,
-            .commit-date {
-                color: var(--color-secondary);
-            }
-        }
-
-        th {
-            position: sticky;
-            top: 0;
-            background-color: black;
-        }
-
-        tr.active td {
-            background-color: #222;
-        }
-    }
-
-    .commit-detail {
-        width: 25%;
-
-        .commit-detail-content {
-            position: sticky;
-            top: 0;
-        }
-    }
-}
-
-svg {
-    background-color: transparent;
-
-    .commit-node {
-        fill: #555;
-    }
-
-    .link {
-        stroke: #aaa;
-        stroke-width: 2px;
-        opacity: 0.6;
-        fill: #555;
-    }
-}
 :root {
     --commit-history-item-height: 10px;
 }
+
 .commit-history-graph {
     display: flex;
     gap: 5px;
+    max-width: 500px;
+
+    svg {
+        background-color: transparent;
+
+        .commit-node {
+            fill: #555;
+        }
+
+        .link {
+            stroke: #aaa;
+            stroke-width: 2px;
+            opacity: 0.6;
+            fill: #555;
+        }
+    }
 
     .commit-history-labels {
+        max-width: 400px;
+
         .commit-history-label-item {
             line-height: var(--commit-history-item-height);
             height: var(--commit-history-item-height);
--- app/components/repo/graph-entry.vue
+++ app/components/repo/graph-entry.vue
@@ -7,7 +7,7 @@
             {{ commit.headOf }}
         </div>
         <div class="commit-message">
-            {{ commit.message }}
+            {{ message }}
         </div>
         <div class="commit-timestamp">
             <timeago :date="commit.timestamp" />
@@ -21,6 +21,8 @@ import type { HistoryCommit } from '#shared/types'
 const { commit } = defineProps<{ commit: HistoryCommit }>()
 
 const sha = computed(() => commit.sha.substring(0, 7))
+
+const message = computed(() => commit.message.split('\n')[0] ?? '<no message>')
 </script>
 
 <style lang="scss">
@@ -33,6 +35,17 @@ const sha = computed(() => commit.sha.substring(0, 7))
         border: 1px solid var(--color-secondary);
         border-radius: 4px;
         line-height: 15px;
+    }
+
+    .commit-message {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        max-width: 200px;
+    }
+
+    .commit-message {
+        white-space: nowrap;
     }
 
     .commit-sha,
