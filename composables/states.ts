57202972b35166668b280f6ac16985787b41d5b7
Christian Gröber <swiss8oy.chg@gmail.com>
2026-06-18 18:57:34 +0200

    chore: remove selectedFont state

--- composables/states.ts
+++ composables/states.ts
@@ -2,12 +2,9 @@ import { useState } from 'nuxt/app'
 
 type ForgeState = {
     viewingRepo?: string
-    selectedFont: string
 }
 
 export const useForgeState = () =>
     useState<ForgeState>('forge', () => {
-        return {
-            selectedFont: 'Jetbrains Mono',
-        }
+        return {}
     })
