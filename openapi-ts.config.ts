863efdf6319ae6683cc633349ee6bf3cca115bc6
Christian Gröber <swiss8oy.chg@gmail.com>
2026-06-20 11:42:19 +0200

    chore: add wakapi spec, regenerate

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from '@hey-api/openapi-ts'
 
-export default defineConfig({
-    input: 'https://git.pixlmint.ch/swagger.v1.json',
-    output: 'lib/generated',
-})
+export default defineConfig([
+    {
+        input: 'https://git.pixlmint.ch/swagger.v1.json',
+        output: 'lib/forgejo',
+    },
+    {
+        input: 'https://wakapi.pixlmint.ch/swagger-ui/swagger-ui/doc.json',
+        output: 'lib/wakapi',
+    },
+])
