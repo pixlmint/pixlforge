9f0518a4277d06e3f167b365f6e51723ecccc339
Christian Gröber <swiss8oy.chg@gmail.com>
2026-06-20 14:11:32 +0200

    chore: remove runtime configs

--- lib/wakapi-config.ts
+++
@@ -1,6 +0,0 @@
-import type { CreateClientConfig } from './wakapi/client.gen'
-
-export const createClientConfig: CreateClientConfig = (config) => ({
-    ...config,
-    baseUrl: 'https://example.com',
-})
