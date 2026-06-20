c8c3bcc90e09a37f17fa6adf42725e084469c85f
Christian Gröber <swiss8oy.chg@gmail.com>
2026-06-20 11:26:18 +0200

    chore: ditch tanstack plugin, fix openapi-ts config

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from '@hey-api/openapi-ts';
+import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
     input: 'https://git.pixlmint.ch/swagger.v1.json',
-    output: 'server/lib/api/generated',
-    plugins: [
-        '@hey-api/client-fetch',
-        '@tanstack/vue-query',
-    ],
+    output: 'lib/generated',
 })
